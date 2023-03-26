03fc739d65a5c1e87697f7d6547291caaf37c0b5
arsitmaniya <Amaniya8692@conestogac.on.ca>
2023-03-25 23:59:39 -0400

    db context created

---
+++ Assignment3/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Assignment3.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
